a43d91835ccf0f8d1df6b26f2e3d1efa17a62b70
shivakrishna-dotcom <shivakrishna0227@gmail.com>
2025-12-09 21:05:00 +0530

    naturalproducts&registration form
    naturalproducts&registration form

---
+++ src/main/java/com/shiva/Asvmall/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.shiva.Asvmall.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.shiva.Asvmall.entity.UserEntity;
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    UserEntity findByMobile(String mobile);
+}
